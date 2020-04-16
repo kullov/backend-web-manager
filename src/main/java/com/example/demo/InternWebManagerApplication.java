72902b3379ee15756f6dabbcdb6c6fd4960c30a0
nws-ngatt <ngatt@newwave.vn>
2020-04-16 11:00:50 +0700

    add vue project

--- src/main/java/com/example/demo/InternWebManagerApplication.java
+++ src/main/java/com/example/demo/InternWebManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendWebManagerApplication {
+public class InternWebManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendWebManagerApplication.class, args);
+		SpringApplication.run(InternWebManagerApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/InternWebManagerApplicationTests.java
+++ src/test/java/com/example/demo/InternWebManagerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendWebManagerApplicationTests {
+class InternWebManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
