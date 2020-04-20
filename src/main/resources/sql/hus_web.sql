9e722e1fd150871ca824d8fc428664c2da7021e8
nws-ducnt <ducnt@newwave.vn>
2020-04-20 23:24:42 +0700

    add sql

---
+++ src/main/resources/sql/hus_web.sql
@@ -0,0 +1,255 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2019 at 03:36 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hus_web`
+--
+CREATE DATABASE IF NOT EXISTS `hus_web` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_vietnamese_ci;
+USE `hus_web`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ability_dictionary`
+--
+
+DROP TABLE IF EXISTS `ability_dictionary`;
+CREATE TABLE IF NOT EXISTS `ability_dictionary` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `description` varchar(50) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type_id` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ability_type`
+--
+
+DROP TABLE IF EXISTS `ability_type`;
+CREATE TABLE IF NOT EXISTS `ability_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `intern_ability`
+--
+
+DROP TABLE IF EXISTS `intern_ability`;
+CREATE TABLE IF NOT EXISTS `intern_ability` (
+  `intern_id` int(11) NOT NULL,
+  `ability_id` int(11) NOT NULL,
+  `rate` varchar(10) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  KEY `intern_id` (`intern_id`),
+  KEY `ability_id` (`ability_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `intern_profile`
+--
+
+DROP TABLE IF EXISTS `intern_profile`;
+CREATE TABLE IF NOT EXISTS `intern_profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(15) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `first_name` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `last_name` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `join_date` date NOT NULL,
+  `class_name` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `avatar` varchar(200) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organization_profile`
+--
+
+DROP TABLE IF EXISTS `organization_profile`;
+CREATE TABLE IF NOT EXISTS `organization_profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `employee_count` int(11) NOT NULL,
+  `gross_revenue` int(11) NOT NULL,
+  `address` varchar(50) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `contact` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `tax_number` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `intern_id` int(11) NOT NULL,
+  `request_id` int(11) NOT NULL,
+  `date_created` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `intern_id` (`intern_id`),
+  KEY `request_id` (`request_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+DROP TABLE IF EXISTS `request`;
+CREATE TABLE IF NOT EXISTS `request` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `organization_id` int(11) NOT NULL,
+  `position` varchar(11) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `amount` int(11) NOT NULL,
+  `date_created` date NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `organization_id` (`organization_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request_ability`
+--
+
+DROP TABLE IF EXISTS `request_ability`;
+CREATE TABLE IF NOT EXISTS `request_ability` (
+  `request_id` int(11) NOT NULL,
+  `ability_id` int(11) NOT NULL,
+  `ability_required` int(11) NOT NULL,
+  `description` int(11) NOT NULL,
+  KEY `request_id` (`request_id`),
+  KEY `ability_id` (`ability_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request_assignment`
+--
+
+DROP TABLE IF EXISTS `request_assignment`;
+CREATE TABLE IF NOT EXISTS `request_assignment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `request_id` int(11) NOT NULL,
+  `intern_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `status` int(11) NOT NULL,
+  `date_created` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `request_id` (`request_id`),
+  KEY `intern_id` (`intern_id`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+CREATE TABLE IF NOT EXISTS `status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_profile`
+--
+
+DROP TABLE IF EXISTS `teacher_profile`;
+CREATE TABLE IF NOT EXISTS `teacher_profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `contact` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ability_dictionary`
+--
+ALTER TABLE `ability_dictionary`
+  ADD CONSTRAINT `ability_dictionary_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `ability_type` (`id`);
+
+--
+-- Constraints for table `intern_ability`
+--
+ALTER TABLE `intern_ability`
+  ADD CONSTRAINT `intern_ability_ibfk_1` FOREIGN KEY (`intern_id`) REFERENCES `intern_profile` (`id`),
+  ADD CONSTRAINT `intern_ability_ibfk_2` FOREIGN KEY (`ability_id`) REFERENCES `ability_dictionary` (`id`);
+
+--
+-- Constraints for table `register`
+--
+ALTER TABLE `register`
+  ADD CONSTRAINT `register_ibfk_1` FOREIGN KEY (`intern_id`) REFERENCES `intern_profile` (`id`),
+  ADD CONSTRAINT `register_ibfk_2` FOREIGN KEY (`request_id`) REFERENCES `request` (`id`);
+
+--
+-- Constraints for table `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`organization_id`) REFERENCES `organization_profile` (`id`);
+
+--
+-- Constraints for table `request_ability`
+--
+ALTER TABLE `request_ability`
+  ADD CONSTRAINT `request_ability_ibfk_1` FOREIGN KEY (`request_id`) REFERENCES `request` (`id`),
+  ADD CONSTRAINT `request_ability_ibfk_2` FOREIGN KEY (`ability_id`) REFERENCES `ability_dictionary` (`id`);
+
+--
+-- Constraints for table `request_assignment`
+--
+ALTER TABLE `request_assignment`
+  ADD CONSTRAINT `request_assignment_ibfk_1` FOREIGN KEY (`request_id`) REFERENCES `request` (`id`),
+  ADD CONSTRAINT `request_assignment_ibfk_2` FOREIGN KEY (`intern_id`) REFERENCES `intern_profile` (`id`),
+  ADD CONSTRAINT `request_assignment_ibfk_3` FOREIGN KEY (`status`) REFERENCES `status` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
